0b48b13083738f0c48cdc04867b7c77128e59874
Patrice HANNE <patrice.hanne@gmail.com>
2025-01-09 18:51:57 +0100

    Ajoutez des fichiers projet.

---
+++ Terre00/Terre00.cs
@@ -0,0 +1,29 @@
+﻿/* 00 - L’ALPHABET - 00
+
+Créez un programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne.
+Exemples d’utilisation :
+    $> python exo.py
+    abcdefghijklmnopqrstuvwxyz
+    $>
+Attention : votre programme devra utiliser une boucle. 
+
+|||||||||||||||||||||||| Objectif : apprendre qu'on peut récupérer simplement chaque caractère a l'aide d'un code ASCII ||||||||||||||||||||||||
+*/
+
+// Méthode 0 (Ascii) :
+
+using System.Reflection;
+
+class Terre00
+{
+    static void Main()
+    {
+        string alphabet = ""; // Création d'une chaîne pour stocker l'alphabet
+
+        for (int i = 97; i <= 122; i++) // Utilisation d'une boucle pour concaténer les lettres à la chaîne alphabet
+        {
+            alphabet += (char)i; // Incrémentation de la variable alphabet
+        }
+        Console.WriteLine(alphabet); // Affichage de l'alphabet sur une seule ligne
+    }
+}
