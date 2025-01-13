69278ae9ab470af9a02a181cc3854bc8d6c35a43
Patrice Hanne <patrice.hanne@gmail.com>
2025-01-13 23:41:59 +0100

    Terre03

---
+++ Epreuves_2025/Terre03/Terre03.cs
@@ -0,0 +1,24 @@
+﻿/* 03 - L’ALPHABET A PARTIR DE - 03
+
+Créez un programme qui affiche l’alphabet à partir de la lettre donnée en argument en lettres minuscules suivi d’un retour à la ligne.
+Exemples d’utilisation :
+    $> python exo.py n
+    nopqrstuvwxyz
+    $>
+Attention : votre programme devra utiliser une boucle.
+
+|||||||||||||||||||||||| Correction : environment.getCommandLineArgs (pour récupérer mes arguments) ||||||||||||||||||||||||
+|||||||||||||||||||||||| Correction : chaine vide + incrémenter ||||||||||||||||||||||||
+*/
+
+class Terre03
+{
+    static void Main()
+    {
+        Console.WriteLine("==========-==========-==========-==========-==========");
+        Console.WriteLine("Bonjour ! \nBienvenue dans ce programme. \nCette console donne l'alphabet à partir de la lettre donnée en argument.");
+        Console.WriteLine("==========-==========-==========-==========-==========");
+
+        
+    }
+}
