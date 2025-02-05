3364684c72290947731af3553726861dd99c7858
Patrice Hanne <patrice.hanne@gmail.com>
2025-02-05 23:58:32 +0100

    ras31

--- Epreuves_2025/Terre03/Terre03.cs
+++ Epreuves_2025/Terre03/Terre03.cs
@@ -32,9 +32,7 @@ class Terre03
             }
             else
             {
-                Console.WriteLine("\n-------------=-------------=-------------=-------------=-------------");
-                Console.WriteLine("Entrée non valide. Vous avez entré un chiffre ou plusieurs caractères.");
-                Console.WriteLine("-------------=-------------=-------------=-------------=-------------\n");
+
             }
         }
     }
