d975c0ba1f4f0599e8089e0fbf93dc9927fa8885
Patrice Hanne <patrice.hanne@gmail.com>
2025-03-05 00:02:53 +0100

    tedc6

--- Epreuves_2025/Terre08/Terre08.cs
+++ Epreuves_2025/Terre08/Terre08.cs
@@ -1,10 +1,68 @@
-﻿namespace Terre08
+﻿/* 08 - PUISSANCE D'UN NOMBRE - 08
+
+Créez un programme qui affiche le résultat d’une puissance.
+L’exposant ne pourra pas être négatif.
+Exemples d’utilisation :
+    $> node exo.js 2 3
+    8
+Attention : je compte sur vous pour gérer les potentielles erreurs d’arguments.
+Fonctions interdites: 
+-La fonction pow
+
+ */
+
+class Terre08
 {
-    internal class Terre08
+    static void Main()
     {
-        static void Main(string[] args)
+        //Console.WriteLine("==========-==========-==========-==========-==========");
+        //Console.WriteLine("Bonjour ! \nBienvenue dans ce programme.");
+        //Console.WriteLine("Cette console affiche le résultat d’une puissance entre un nombre et un exposant.");
+        //Console.WriteLine("==========-==========-==========-==========-==========");
+
+        do
         {
-            Console.WriteLine("Hello, World!");
+            // On définie le NOMBRE
+            Console.WriteLine("Veuillez entrer un NOMBRE : (ou 'exit' pour quitter)");
+            string baseNumberInput = Console.ReadLine();
+
+            if (baseNumberInput.ToLower() == "exit")
+                break;
+
+            if (double.TryParse(baseNumberInput, out double baseNumber))
+            {
+                // On définie l'EXPOSANT
+                Console.WriteLine("Veuillez entrer un EXPOSANT : (ou 'exit' pour quitter)");
+                string exponentInput = Console.ReadLine();
+
+                if (exponentInput.ToLower() == "exit")
+                    break;
+
+                if (int.TryParse(exponentInput, out int exponent))
+                {
+                    Console.WriteLine("Le résultat du calcul de puissance est : " + Puissance(baseNumber, exponent));
+                }
+                else
+                {
+                    Console.WriteLine("Attention ! Vous n'avez pas entrer un exposant valide... Concentrez-vous ! \n|||||||||||||||| On recommence : ||||||||||||||||");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Attention ! Vous n'avez pas entrer un nombre Valide... Pas de blagues !");
+            }
+            Console.WriteLine("----------=----------=----------=----------=----------");
+        } while (true);
+    }
+
+    // Méthodes :
+    static double Puissance(double baseNumber, int exponent) 
+    {
+        double result = 1;
+        for (int i = 0; i < exponent; i++)
+        {
+            result *= baseNumber;
         }
+        return result;
     }
 }
--- Epreuves_2025/Terre09/Terre09.cs
+++ Epreuves_2025/Terre09/Terre09.cs
@@ -1,10 +1,71 @@
-﻿namespace Terre09
+﻿/* 09 - RACINE CARRE D'UN NOMBRE - 09
+
+Créez un programme qui affiche la racine carrée d’un entier positif.
+Exemples d’utilisation :
+    $> node exo.js 9
+    3
+Attention : je compte sur vous pour gérer les potentielles erreurs d’arguments.
+Fonctions interdites: 
+-La fonction sqrt
+ */
+
+class Terre09
 {
-    internal class Terre09
+    static void Main()
     {
-        static void Main(string[] args)
+        //Console.WriteLine("==========-==========-==========-==========-==========");
+        //Console.WriteLine("Bonjour ! \nBienvenue dans ce programme.");
+        //Console.WriteLine("Cette console affiche la Racine Carré d'un nombre, entier et positif :");
+        //Console.WriteLine("==========-==========-==========-==========-==========");
+
+        // Qu'est ce qu'une racine carré mathématiquement parlant : 
+        // Décomposer les étapes : 
+        // Par exemple : Racine de 4 est 2. Car 2 au carré = 4
+        // Autre exemple : Racine de 16 = 4. Car 4 x 4 = 16
+        // Autre exemple : Racine de 100 = 10. Car 10 x 10 = 100
+        // 
+        // Déjà il faut vérifier que le nombre donné est un entier positif.
+
+        do
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Veuillez entrer un nombre : (ou 'exit' pour quitter)");
+            string baseNumberInput = Console.ReadLine();
+
+            if (baseNumberInput.ToLower() == "exit")
+                break;
+
+            if (double.TryParse(baseNumberInput, out double baseNumber) && baseNumber > 0)
+            {
+                RacineCarre((int)baseNumber);
+                // Console.WriteLine(RacineCarre(Convert.ToInt32(baseNumber)).ToString("F2"));
+                // Console.WriteLine("Le résultat du calcul de la Racine Carré est : " + RacineCarre((int)baseNumber));
+                // Console.WriteLine("Le résultat du calcul de la Racine Carré est : " + RacineCarre(baseNumber).ToString("F2"));
+            }
+            else
+            {
+                Console.WriteLine("- /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ -");
+                Console.WriteLine("- Attention ! Veuillez entrer un nombre ENTIER et POSITIF svp -");
+                Console.WriteLine("- /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ /!\\ -");
+            }
+            Console.WriteLine("----------=----------=----------=----------=----------");
+
+        } while (true);
+    }
+
+    // Méthodes :
+    static double RacineCarre(int baseNumber)
+    {
+        double result = Math.Pow(baseNumber, 0.5); // 0.5 et non pas 0,5 !
+
+        if (result % 1 == 0)
+        {
+            Console.WriteLine("Le résultat du calcul de la Racine Carré est : " + result.ToString("F0")); // pour afficher le résultat sans décimal
         }
+        else
+        {
+            Console.WriteLine("Le résultat du calcul de la Racine Carré est : " + result.ToString("F2")); // pour afficher le résultat avec 2 décimals
+        }
+        return result;
+        // return (int)result;
     }
 }
