1aa536e6227f4bded1213a363b3a7be3ab0ea230
Patrice Hanne <patrice.hanne@gmail.com>
2025-03-06 15:56:26 +0100

    ui

--- Epreuves_2025/Terre14/Terre14.cs
+++ Epreuves_2025/Terre14/Terre14.cs
@@ -13,19 +13,12 @@ Fonctions interdites:
 -La fonction sort
 */
 
-
 class Terre14
 {
     static void Main()
-    {
-        //Console.WriteLine("==========-==========-==========-==========-==========");
-        //Console.WriteLine("Bonjour ! \nBienvenue dans ce programme.");
-        //Console.WriteLine("Cette console récupère en [Entrée] une liste de nombres entier et renvoie en [Sortie] si ceux-ci sont triés ou pas. \nFournissez des nombres séparés par un espace ex: 5 10 3 25 105 9");
-        //Console.WriteLine("==========-==========-==========-==========-==========");
-        bool continuer = true;
+    { bool continuer = true;
         do
         {
-            // Demander à l'utilisateur d'entrer une liste de nombres entiers séparés par des espaces
             Console.WriteLine("Veuillez entrer une liste de nombres entiers séparés par un espace (ou 'exit' pour quitter) :");
             string input = Console.ReadLine();
 
@@ -33,8 +26,6 @@ class Terre14
             {
                 break;
             }
-
-            // Diviser la chaîne d'entrée en un tableau de chaînes en utilisant l'espace comme délimiteur
             string[] numbersString = input.Split(' ');
 
             // Convertir les chaînes en nombres entiers
@@ -48,13 +39,11 @@ class Terre14
                     break;
                 }
             }
-
             if (!continuer)
             {
                 continue;
             }
 
-            // Vérifier si la liste de nombres est triée
             bool sorted = true;
             for (int i = 1; i < numbers.Length; i++)
             {
@@ -65,7 +54,6 @@ class Terre14
                 }
             }
 
-            // Afficher le résultat
             if (sorted)
             {
                 Console.WriteLine("Les nombres sont triés.");
